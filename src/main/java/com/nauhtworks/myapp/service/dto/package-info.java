2d892ef80eb73c0d171d447b722d4db8eaa795da
Thuan Saraiva <thuan.saraiva@pathfind.com.br>
2021-09-23 18:35:16 -0300

    reformat project

--- src/main/java/com/nauhtworks/myapp/service/dto/package-info.java
+++
@@ -1,4 +0,0 @@
-/**
- * Data Transfer Objects.
- */
-package com.nauhtworks.myapp.service.dto;
--- src/main/webapp/app/routes/admin/__layout.svelte
+++
@@ -1,7 +0,0 @@
-<script>
-	import RoleGuard from '$lib/auth/RoleGuard.svelte'
-</script>
-
-<RoleGuard role="ADMIN" showAccessDeniedMessage="true">
-	<slot />
-</RoleGuard>
